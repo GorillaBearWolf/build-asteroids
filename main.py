69b6745340925481385789bb6adc38aada001702
mrefsnider <mrefsnider@orangetheory.com>
2024-09-04 00:17:17 -0400

    Restrict framerate to 60fps

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from constants import *
 def main():
     pygame.init()
 
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
@@ -18,7 +20,7 @@ def main():
 
         screen.fill(0)
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
