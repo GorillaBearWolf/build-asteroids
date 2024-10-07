2f308c018b361adf49d7e1d7072ec33a904e5dd7
mrefsnider <mrefsnider@orangetheory.com>
2024-10-07 17:12:09 -0400

    Updates

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -10,6 +11,7 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
@@ -21,6 +23,9 @@ def main():
         screen.fill(0)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+        player.draw(screen)
+
+
 
 
 if __name__ == "__main__":
